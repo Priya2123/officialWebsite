6f2494c14ea6c2dd4af741545cb9ead5a33ef708
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-09-19 23:34:46 +0530

    toggle error

--- src/App.js
+++ src/App.js
@@ -9,23 +9,47 @@ import Project from "./pages/Project";
 import Resources from "./pages/Resources";
 import Team from "./pages/Team";
 import Chatbot from "./components/Chatbot";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
+import style from "styled-theming";
+import useTheme from "./useTheme";
+
+const getBackground = style("mode", {
+  light: "#EEE",
+  dark: "#111",
+});
+const getForeground = style("mode", {
+  light: "#111",
+  dark: "#EEE",
+});
+const GlobalStyle = createGlobalStyle`
+body{ 
+  background-color: ${getBackground};
+  color: ${getForeground};
+}
+`;
 
 const App = () => {
+  const theme = useTheme();
   return (
-    <Router>
-      <div className="App">
-        <MobileNav />
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/events" component={Events} />
-          <Route exact path="/projects" component={Project} />
-          <Route exact path="/resources" component={Resources} />
-          <Route exact path="/team" component={Team} />
-        </Switch>
-        <Chatbot />
-      </div>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyle />
+        <Router>
+          <div className="App">
+            <MobileNav />
+            <Nav />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/events" component={Events} />
+              <Route exact path="/projects" component={Project} />
+              <Route exact path="/resources" component={Resources} />
+              <Route exact path="/team" component={Team} />
+            </Switch>
+            <Chatbot />
+          </div>
+        </Router>
+      </>
+    </ThemeProvider>
   );
 };
 
---
+++ src/useTheme.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+import storage from "local-storage-fallback";
+
+export default function useTheme(defaultTheme = { mode: "light" }) {
+  const getInitialTheme = () => {
+    const savedTheme = storage.getItem("theme");
+    return savedTheme ? JSON.parse(savedTheme) : defaultTheme;
+  };
+  const [theme, _setTheme] = useState(getInitialTheme);
+  useEffect(() => {
+    storage.setItem("theme", JSON.stringify(theme));
+  }, [theme]);
+  return {
+    ...theme,
+    setTheme: ({ setTheme, ...theme }) => _setTheme(theme),
+  };
+}
